6abfe4489d49360b3e0676e8e6c18677098def31
jettob0i <kage.zama.04@gmail.com>
2024-04-24 15:21:27 +0300

    Note.kt2

--- app/src/main/java/com/topic2/android/notes/ui/components/Note.kt
+++ app/src/main/java/com/topic2/android/notes/ui/components/Note.kt
@@ -2,6 +2,9 @@ package com.topic2.android.notes.ui.components
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.Checkbox
@@ -14,15 +17,25 @@ import com.topic2.android.notes.theme.rwGreen
 
 @Composable
 fun Note() {
-    Box(
-        modifier = Modifier
-            .size(40.dp)
-            .background(rwGreen)
-    )
-    Text(text = "Заголовок", maxLines = 1)
-    Text(text = "Содержание", maxLines = 1)
-    Checkbox(
-        checked = false, onCheckedChange = { },
-        modifier = Modifier.padding(start = 8.dp)
-    )
+    Row(modifier = Modifier.fillMaxWidth()) {
+        Box(
+            modifier = Modifier
+                .size(40.dp)
+                .background(rwGreen)
+        )
+        Column(modifier = Modifier.weight(1f)) {
+            Text(text = "Заголовок", maxLines = 1)
+            Text(text = "Содержание", maxLines = 1)
+        }
+        Checkbox(
+            checked = false,
+            onCheckedChange = { },
+            modifier = Modifier.padding(start = 8.dp)
+        )
+    }
+}
+@Preview
+@Composable
+private fun NotePreview() { Note()
+
 }
