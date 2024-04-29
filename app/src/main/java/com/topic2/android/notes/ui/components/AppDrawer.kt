b99544aa57b1642a9eb03ca623d834f70b58ca68
jettob0i <kage.zama.04@gmail.com>
2024-04-29 13:55:25 +0300

    LightDark theme

--- app/src/main/java/com/topic2/android/notes/ui/components/AppDrawer.kt
+++ app/src/main/java/com/topic2/android/notes/ui/components/AppDrawer.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
+import androidx.compose.material.Switch
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Home
@@ -23,6 +24,7 @@ import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.topic2.android.notes.theme.NotesTheme
+import com.topic2.android.notes.theme.NotesThemeSettings
 
 @Composable
 private fun AppDrawerHeader() {
@@ -39,6 +41,39 @@ private fun AppDrawerHeader() {
             modifier = Modifier
                 .align(alignment = Alignment.CenterVertically)
         )
+    }
+}
+
+@Composable
+private fun LightDarkThemeItem() {
+    Row(
+        Modifier
+            .padding(8.dp)
+    ) {
+        Text(
+            text = "Включить тёмную тему",
+            style = MaterialTheme.typography.body2,
+            color = MaterialTheme.colors.onSurface.copy(alpha = 0.6f),
+            modifier = Modifier
+                .weight(1f)
+                .padding(start = 8.dp, top = 8.dp, end = 8.dp, bottom = 8.dp)
+                .align(alignment = Alignment.CenterVertically)
+        )
+        Switch(
+            checked = NotesThemeSettings.isDarkThemeEnabled,
+            onCheckedChange = { NotesThemeSettings.isDarkThemeEnabled = it },
+            modifier = Modifier
+                .padding(start = 8.dp, end = 8.dp)
+                .align(alignment = Alignment.CenterVertically)
+        )
+    }
+}
+
+@Preview
+@Composable
+fun LightDarkThemeItemPreview() {
+    NotesTheme {
+        LightDarkThemeItem()
     }
 }
 
