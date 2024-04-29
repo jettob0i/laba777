e790dea551ac7363aa3674d605ffe22c4b09755a
jettob0i <kage.zama.04@gmail.com>
2024-04-29 14:41:15 +0300

    padding

--- app/src/main/java/com/topic2/android/notes/ui/components/NoteColor.kt
+++ app/src/main/java/com/topic2/android/notes/ui/components/NoteColor.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.runtime.Composable
@@ -18,6 +19,7 @@ import androidx.compose.ui.unit.dp
 fun NoteColor() {
     Box(
         modifier = Modifier
+            .padding(4.dp)
             .size(40.dp)
             .clip(CircleShape)
             .background(Color.Red)
