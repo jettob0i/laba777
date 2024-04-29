c7d9d4fe76f0683ead683c1056c88ab897b9d9aa
jettob0i <kage.zama.04@gmail.com>
2024-04-29 14:53:15 +0300

    savenotescreen

--- app/src/main/java/com/topic2/android/notes/ui/screens/SaveNoteScreen.kt
+++ app/src/main/java/com/topic2/android/notes/ui/screens/SaveNoteScreen.kt
@@ -1,43 +1,45 @@
 package com.topic2.android.notes.ui.screens
 
-import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Menu
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.ColorFilter
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
-import com.topic2.android.notes.theme.NotesTheme
+import androidx.compose.ui.unit.sp
+import com.topic2.android.notes.domain.model.ColorModel
+import com.topic2.android.notes.ui.components.NoteColor
+import com.topic2.android.notes.util.fromHex
 
 @Composable
-private fun AppDrawerHeader() {
-    Row(modifier = Modifier.fillMaxWidth()) {
-        Image(
-            imageVector = Icons.Filled.Menu,
-            contentDescription = "Drawer Header Icon",
-            colorFilter = ColorFilter
-                .tint(MaterialTheme.colors.onSurface),
-            modifier = Modifier.padding(16.dp)
+fun ColorItem(
+    color: ColorModel,
+    onColorSelect: (ColorModel) -> Unit
+) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .clickable(
+                onClick = {
+                    onColorSelect(color)
+                }
+            )
+    ) {
+        NoteColor(
+            modifier = Modifier
+                .padding(10.dp), color = Color.fromHex(color.hex), size = 80.dp,
+            border = 2.dp
         )
         Text(
-            text = "Заметки",
+            text = color.name,
+            fontSize = 22.sp,
             modifier = Modifier
-                .align(alignment = Alignment.CenterVertically)
+                .padding(horizontal = 16.dp)
+                .align(Alignment.CenterVertically)
         )
-    }
-}
-
-@Preview
-@Composable
-fun AppDrawerHeaderPreview() {
-    NotesTheme {
-        AppDrawerHeader()
     }
 }
